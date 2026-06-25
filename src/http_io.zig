da78ec4d385a4e5f0833f8a15caa09531a1a7941
Zeref <zeref18.drgnl@gmail.com>
2026-06-25 10:04:09 +0530

    listening to the client

--- src/http_io.zig
+++ src/http_io.zig
@@ -2,24 +2,26 @@ const std = @import("std");
 const print = std.debug.print;
 
 export fn http_listener(io: *const std.Io) void {
-    // const gpa = init.gpa;
-
     const addr = std.Io.net.IpAddress.parse("0.0.0.0", 3636) catch |err| {
         print("Failed to parse IP Address: {}\n", .{err});
         return;
     };
+
     var server = addr.listen(io.*, .{}) catch |err| {
         print("Unable to listen to server: {}\n", .{err});
         return;
     };
 
-    std.debug.print("Starting to listen on port {}", .{3636});
+    std.debug.print("TCP listening on port {}\n", .{3636});
 
     while (true) {
         var client = server.accept(io.*) catch |err| {
             print("Unable to accept server requests: {}\n", .{err});
             return;
         };
+
+        print("listening to the client\n", .{});
+
         defer client.close(io.*);
     }
 }
