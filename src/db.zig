f4837eb7031000878bd73f0d7a003525b97c78fc
Zeref <zeref18.drgnl@gmail.com>
2026-06-26 11:42:23 +0530

    added the DB struct

--- src/db.zig
+++ src/db.zig
@@ -2,4 +2,13 @@
 //! the /get-contact?name=hunter will return data associated to hunter
 //! /update-contact will take the key-value pair for the target contact and update it
 //! /upload-contact will add a new row to the db
-//! and lastly, /delete-contact will delete a specified
+//! and lastly, /delete-contact will delete a specified contact
+
+pub const DB = struct {
+    pub fn new() DB {}
+
+    pub fn fetchContact(contact: []const u8) !void {}
+    pub fn updateContact(contact: []const u8, ph: []const u8) !void {}
+    pub fn uploadContact(contact: []const u8, ph: []const u8) !void {}
+    pub fn delContact(contact: []const u8) !void {}
+};
