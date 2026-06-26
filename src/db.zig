1f8e4b4741bddaba6a5d191f304abd3e5476ad01
Zeref <zeref18.drgnl@gmail.com>
2026-06-26 12:35:01 +0530

    added db related fns

--- src/db.zig
+++ src/db.zig
@@ -3,9 +3,18 @@
 //! /update-contact will take the key-value pair for the target contact and update it
 //! /upload-contact will add a new row to the db
 //! and lastly, /delete-contact will delete a specified contact
+const std = @import("std");
 
 pub const DB = struct {
-    pub fn new() DB {}
+    endpoint: []const u8,
+
+    pub fn new(endpoint: []const u8) !DB {
+        // Connect to the db endpoint
+
+        // Make sure the connection is established for long time
+
+        return DB{ .endpoint = endpoint };
+    }
 
     pub fn fetchContact(contact: []const u8) !void {}
     pub fn updateContact(contact: []const u8, ph: []const u8) !void {}
